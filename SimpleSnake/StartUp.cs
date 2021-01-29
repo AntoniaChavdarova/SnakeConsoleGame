0f92e746b9eee6418562b904110839e89114f5ad
AntoniaChavdarova <tonichavdarova@abv.bg>
2021-01-29 16:08:38 +0200

    Update

---
+++ SimpleSnake/StartUp.cs
@@ -0,0 +1,18 @@
+﻿namespace SimpleSnake
+{
+    using SimpleSnake.GameObjects;
+    using Utilities;
+
+    public class StartUp
+    {
+        public static void Main()
+        {
+            ConsoleWindow.CustomizeConsole();
+
+            Point point = new Point(0, 0);
+
+            point.Draw('@');
+
+        }
+    }
+}
